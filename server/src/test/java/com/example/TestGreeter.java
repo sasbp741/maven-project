f1520baa785704532eb50b9b87ca13518730666c
sasbp741 <78941722+sasbp741@users.noreply.github.com>
2021-07-02 11:14:11 +0200

    test3 fail Tunit

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = 1;
+    
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
