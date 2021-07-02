99453813f6ba65a4a5266afef3b913bf6dcc9812
sasbp741 <78941722+sasbp741@users.noreply.github.com>
2021-07-02 11:38:02 +0200

    test5 fail Tunit

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -17,17 +17,29 @@ public class TestGreeter {
     greeter = new Greeter();
   }
 
+/*  @Test
+  public void greetShouldIncludeTheOneBeingGreeted() {
+    String someone = "World";
+    assertThat(greeter.greet(someone), containsString(someone));
+    //test1 : doit inclure la string someone
+  }
+  */
+
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
+     String someone2 = "ola";
 
-    assertThat(greeter.greet(someone), containsString(someone));
+    assertThat(greeter.greet(someone), containsString(someone2));
+    //essai de rendre le test failed, en rajoutant une 2eme personne
   }
-
+  
   @Test
   public void greetShouldIncludeGreetingPhrase() {
-    String someone = "Boooooooooooooooooooooooob";
+    String someone = "Boooooooooooooooooooooooooooooooooooooooooooooooooooooooooob";
 
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length()))); 
+    // test2 ce qui doit être retourné, doit être plus grand que la taille du "Hello babacar, Bob" - ça permet de voir qu'il y a un mot en plus
+    //il doit dire plus que ce que ce qu'on lui donne cad Bob + Hello babacar
   }
 }
