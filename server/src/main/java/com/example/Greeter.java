716cc2a0c4fd88acc568af3e87fc25690fb0b9cb
sasbp741 <78941722+sasbp741@users.noreply.github.com>
2021-07-02 15:02:57 +0200

    test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,7 +5,10 @@ package com.example;
 /**
  * Info about this package doing something for package-info.java file.
  */
-public class Greeter {  
+public class Greeter {
+      /**
+ * Info about this package doing something for package-info.java file.
+ */
   public Greeter() {
     /**
  * Info about this package doing something for package-info.java file.
@@ -15,7 +18,7 @@ public class Greeter {
     /**
  * @version 3.0
  */
-    return String.format("Hello babacar, %s!", someone);  
+    return String.format("Hello babacar, %s!", someone);
     // le %s affiche ce qu'on a mis dans someone
   }
 }
