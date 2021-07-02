a1b59b7e2b4f8351000d9558b4720415dccd1de6
sasbp741 <78941722+sasbp741@users.noreply.github.com>
2021-07-02 15:16:17 +0200

    test

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,7 +1,7 @@
 package com.example;
 /**
  * <p>
- * De plus, un Zéro a une liste d'amis Zéro. Le membre pourra ajouter ou enlever
+ * De plus, un Zéro a une liste d'amis Zéro.
  * des amis à cette liste.
  * </p>
  * @see SDZLevel
@@ -11,14 +11,14 @@ package com.example;
  */
 public class Greeter {
       /**
- * this is a constructor
+ * this is a constructor.
  */
   public Greeter() {
-  /**
+  }
+/**
  * @param someone name of a person
  *@return greeting string
  */
-  }
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
     // le %s affiche ce qu'on a mis dans someone
