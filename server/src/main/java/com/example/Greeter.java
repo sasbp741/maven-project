2e1251fc762f2a4654bad9b65577b42741e43ac4
sasbp741 <78941722+sasbp741@users.noreply.github.com>
2021-07-02 11:36:20 +0200

    commentaire sur la fonction greet

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,6 +9,6 @@ public class Greeter {
   }
 
   public final String greet(final String someone) {
-    return String.format("Hello babacar, %s!", someone);
+    return String.format("Hello babacar, %s!", someone);  // le %s affiche ce qu'on a mis dans someone
   }
 }
